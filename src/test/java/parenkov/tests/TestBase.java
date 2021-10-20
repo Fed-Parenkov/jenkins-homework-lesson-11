0f7e8678bbbb51facbca33b56e88460ae1e0cca3
ParenkovLocal <parenkov.fedor@gmail.com>
2021-10-20 18:53:54 +0400

    Edit TestBase

--- src/test/java/parenkov/tests/TestBase.java
+++ src/test/java/parenkov/tests/TestBase.java
@@ -10,8 +10,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-import static java.lang.String.format;
-
 
 public class TestBase {
 
@@ -29,20 +27,15 @@ public class TestBase {
         Configuration.browserCapabilities = capabilities;
         Configuration.startMaximized = true;
 
+        // передаем url в виде системной переменной из jenkins
         String url = System.getProperty("url");
-
-        // передаем креды для selenoid из jenkins в виде системных переменных
-//        String login = System.getProperty("login");
-//        String password = System.getProperty("password");
 
         // передаем креды для selenoid из файла .properties
         String login = credentials.login();
         String password = credentials.password();
 
-//        String remote = format("https://%s:%s@%s", login, password, url);
         String remote = "https://" + login + ":" + password + "@" + url;
-//        String remote = "https://" + login + ":" + password + "@" + url;
-        System.out.println(remote);
+
         Configuration.remote = remote;
     }
 
