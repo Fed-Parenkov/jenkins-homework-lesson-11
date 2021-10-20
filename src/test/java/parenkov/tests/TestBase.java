61ad8e07d1648dcb0d1742a5dc400749027393f3
ParenkovLocal <parenkov.fedor@gmail.com>
2021-10-20 16:58:16 +0400

    Added credentials.properties file and interface java class

--- src/test/java/parenkov/tests/TestBase.java
+++ src/test/java/parenkov/tests/TestBase.java
@@ -3,13 +3,20 @@ package parenkov.tests;
 import attachments.Attach;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.CredentialsConfig;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import static java.lang.String.format;
+
 
 public class TestBase {
+    public static CredentialsConfig credentials =
+            ConfigFactory.create(CredentialsConfig.class);
+
     @BeforeAll
     static void setup() {
 
@@ -19,8 +26,14 @@ public class TestBase {
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
         Configuration.startMaximized = true;
-        String url = System.getProperty("rem_url");
-        Configuration.remote = "https://user1:1234@" + url;
+
+        String url = System.getProperty("url");
+        String login = credentials.login();
+        String password = credentials.password();
+
+        String remote = format("https://%s:%s@%s", login, password, url);
+        System.out.println(remote);
+        Configuration.remote = remote;
     }
 
     @AfterEach
